2987c8ff5831d2b1802a8d501ec882dcdf643155
Ryan vanniekerk <iamnayr@gmail.com>
2012-01-23 21:48:18 -0800

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,24 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.0'
+
+group :development, :test do
+  gem 'sqlite3','1.3.5'
+  gem 'rspec-rails', '2.8.1'
+end
+
+group :assets do
+  gem 'sass-rails',   '3.2.3'
+  gem 'coffee-rails', '3.2.1'
+  gem 'uglifier', '1.0.3'
+end
+
+gem 'jquery-rails', '1.0.18'
+
+group :test do
+  gem 'capybara', '1.1.2'
+end
+
+group :production do
+  gem 'pg', '0.12.2'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '9eac3a4fa7f9fcdc722ddff2481a02776312cc39171bfdeaef1edca04a8790cc6ce74e9e8200223d364ed282031b4c02decffc8f241af58e80b46570a729f4b8'
